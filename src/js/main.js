d5c791802dbd19389615df33a5d575eaf84958ac
Le Cao Tung Lam <Kevinle231299@gmail.com>
2023-03-25 22:58:51 +0100

    E-santé projet

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080, function() {
-  console.log('Server listening on port 8080');
-});
+console.log("Server is running...")
