c8487da4f4c37a15a8323a9404d4c098bdda18b0
Le Cao Tung Lam <Kevinle231299@gmail.com>
2023-03-26 11:28:20 +0200

    E-santé projet

--- src/js/server.js
+++ src/js/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'html')));
+app.use("/src",express.static(path.join(__dirname, 'src','js')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../html/index.html'));
